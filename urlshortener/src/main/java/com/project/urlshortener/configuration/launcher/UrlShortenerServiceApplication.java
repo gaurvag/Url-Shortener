dab9bf0c35a816fa8fbcb3b9ae057be3a7b74e07
Gaurav Agrawal <gagrawal@tekion.com>
2024-04-02 01:49:16 +0530

    enabled cross origin requests

--- urlshortener/src/main/java/com/project/urlshortener/configuration/launcher/UrlShortenerServiceApplication.java
+++ urlshortener/src/main/java/com/project/urlshortener/configuration/launcher/UrlShortenerServiceApplication.java
@@ -2,8 +2,11 @@ package com.project.urlshortener.configuration.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.project.urlshortener")
@@ -12,6 +15,17 @@ public class UrlShortenerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
